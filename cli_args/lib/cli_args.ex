7912211e7dee01dd60180fa91d443796cf70fbd5
rschwalk <rschwalk@rschwalk.eu>
2020-04-13 13:28:55 +0200

    Parse arguments

--- cli_args/lib/cli_args.ex
+++ cli_args/lib/cli_args.ex
@@ -13,6 +13,22 @@ defmodule CliArgs do
 
   """
   def main(args) do
-    IO.puts "Hello"
+    args |> parse_arguments |> process
+  end
+
+  def process([]) do
+    IO.puts "No arguments given"
+  end
+  def process({options, pos_args}) do
+    IO.puts "Hello #{options[:name]}"
+    IO.puts "Positinal arguments: #{pos_args}"
+  end
+
+  defp parse_arguments(args) do
+    {options, pos_args, _} = OptionParser.parse(args,
+      aliases: [n: :name],
+      strict: [name: :string]
+    )
+    {options, pos_args}
   end
 end
