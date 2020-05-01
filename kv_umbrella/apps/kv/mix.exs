6e991aadaad21d136233fb9318b85f4ea8e7bdda
rschwalk <rschwalk@rschwalk.eu>
2020-05-01 15:56:57 +0200

    Use Task in the server.

--- kv_umbrella/apps/kv/mix.exs
+++ kv_umbrella/apps/kv/mix.exs
@@ -6,6 +6,10 @@ defmodule KV.MixProject do
       app: :kv,
       version: "0.1.0",
       elixir: "~> 1.10",
+      build_path: "../../README.md",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
--- kv_umbrella/apps/kv_server/lib/kv_server/application.ex
+++ kv_umbrella/apps/kv_server/lib/kv_server/application.ex
@@ -6,9 +6,10 @@ defmodule KVServer.Application do
   use Application
 
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || "4040")
+
     children = [
-      # Starts a worker by calling: KVServer.Worker.start_link(arg)
-      # {KVServer.Worker, arg}
+      {Task, fn -> KVServer.accept(port) end}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
